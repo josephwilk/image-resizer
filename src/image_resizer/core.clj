9172bbc2feec0dea69f7a67d42b6be277bb9c093
josephwilk <joe@josephwilk.net>
2013-05-14 19:51:30 +0200

    resize to file

--- src/image_resizer/core.clj
+++ src/image_resizer/core.clj
@@ -1,13 +1,36 @@
 (ns image-resizer.core
+  (:require
+   [clojure.string :as str])
   (:import
    [java.io File]
    [javax.imageio ImageIO]
    [org.imgscalr Scalr]))
 
+(defn- file-name [file]
+  (first (seq (.split (.getName file) "\\."))))
+
+(defn- path [file]
+  (str/join "/" (butlast (seq (.split (.getAbsolutePath file) "/")))))
+
+(defn extension [file]
+  (last (seq (.split (.getName file) "\\."))))
+
+(defn- new-filename [file dimensions]
+  (str (path file) "/" (file-name file) "_" (str/join "x" dimensions) "." (extension file)))
+
 (defn dimensions [image]
   [(.getWidth image) (.getHeight image)])
 
-(defn resize [file width height]
-  (let [buffered-image (ImageIO/read file)
-        output (File. "resized.jpg")]
-    (Scalr/resize buffered-image width height nil)))
+(defn buffered-image [file] (ImageIO/read file))
+
+(defn resize
+  ([file width]
+     (Scalr/resize (buffered-image file) width nil))
+  ([file width height]
+     (Scalr/resize (buffered-image file) width height nil)))
+
+(defn resize-to-file [file width height]
+  (let [resized-buffered-image (Scalr/resize (buffered-image file) width height nil)
+        new-dimensions (dimensions resized-buffered-image)
+        resized-file (File. (new-filename file new-dimensions))]
+    (ImageIO/write resized-buffered-image (extension file) resized-file)))
--- test/image_resizer/unit/t_core.clj
+++ test/image_resizer/unit/t_core.clj
@@ -2,14 +2,31 @@
   (:import
    [javax.imageio ImageIO])
   (:require
-   [midje.sweet :refer :all]
+   [midje.sweet        :refer :all]
    [image-resizer.core :refer :all]
-   [clojure.java.io :refer :all]))
+   [clojure.java.io    :refer :all]
+   [clojure.java.shell :as shell]))
+
+(namespace-state-changes (after :facts (shell/sh "rm" "-f" "test/fixtures/platypus_10x5.jpg")))
 
 (def test-image
   (file "test/fixtures/platypus.jpg"))
 
-(fact "resize a file to a specific width and height respecting dimensions"
-  (let [resized-image (resize test-image 10 10)]
-    (dimensions (ImageIO/read test-image)) => [600 314]
-    (dimensions resized-image) => [10 5]))
+(facts "resize"
+  (fact "resize a file to a specific width and height respecting dimensions"
+    (let [resized-image (resize test-image 10 10)]
+      (dimensions (ImageIO/read test-image)) => [600 314]
+      (dimensions resized-image) => [10 5]))
+
+  (fact "resize a file to a specific width"
+    (let [resized-image (resize test-image 100)]
+      (dimensions (ImageIO/read test-image)) => [600 314]
+      (first (dimensions resized-image)) => 100)))
+
+(facts "resize-to-file"
+  (fact "resize a file to a specific width and height"
+    (let [resized-image (resize-to-file test-image 10 10)]
+      (.exists (as-file "test/fixtures/platypus_10x5.jpg")) => truthy)))
+
+
+
