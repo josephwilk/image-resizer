05e426dab3967e3c3f68bf1d4fe823b23934afd8
Joseph Wilk <joe@josephwilk.net>
2013-05-16 14:50:32 +0200

    0.1.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject image-resizer "0.1.0-SNAPSHOT"
-  :description "Resize images"
+(defproject image-resizer "0.1.0"
+  :description "Resize/Crop/Pad images"
   :url "https://github.com/josephwilk/image-resizer"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.imgscalr/imgscalr-lib "4.2"]]
