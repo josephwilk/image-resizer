f697e79016bfcefdd24a3b501914f2cedffd46e9
Thiago Nunes <thiagotnunes@gmail.com>
2013-05-23 11:06:07 +0200

    Making sure the crop works for a bigger width and height than the images original dimensions.

--- src/image_resizer/crop.clj
+++ src/image_resizer/crop.clj
@@ -6,16 +6,20 @@
 
 (defn crop-fn [width height]
   (fn [image]
-    (Scalr/crop (buffered-image image) 0 0 width height nil)))
+    (let [buffered (buffered-image image)
+          [original-width original-height] (dimensions buffered)
+          crop-width (min original-width width)
+          crop-height (min original-height height)]
+      (Scalr/crop buffered 0 0 crop-width crop-height nil))))
 
 (defn crop-width-fn [width]
   (fn [image]
     (let [buffered (buffered-image image)
           [_ height] (dimensions buffered)]
-      (Scalr/crop buffered 0 0 width height nil))))
+      ((crop-fn width height) image))))
 
 (defn crop-height-fn [height]
   (fn [image]
     (let [buffered (buffered-image image)
           [width _] (dimensions buffered)]
-      (Scalr/crop buffered 0 0 width height nil))))
+      ((crop-fn width height) image))))
---
+++ test/image_resizer/unit/t_crop.clj
@@ -0,0 +1,29 @@
+(ns image-resizer.unit.t-crop
+  (:require
+   [midje.sweet                :refer :all]
+   [image-resizer.unit.support :refer :all]
+   [image-resizer.crop         :refer :all]))
+
+(facts "about cropping to width and height"
+  (fact "crops the image to the given width and height"
+    ((crop-fn 100 200) test-image) => (dimensions-of [100 200]))
+
+  (fact "does not crop the width when the given width is bigger than images width"
+    ((crop-fn 12345 200) test-image) => (dimensions-of [600 200]))
+
+  (fact "does not crop the height when the given height is bigger than images height"
+    ((crop-fn 100 12345) test-image) => (dimensions-of [100 314])))
+
+(facts "about cropping to width"
+  (fact "crops the image to the given width"
+    ((crop-width-fn 100) test-image) => (dimensions-of [100 314]))
+
+  (fact "does not crop when the given width is bigger than images width"
+    ((crop-width-fn 12345) test-image) => (dimensions-of [600 314])))
+
+(facts "about cropping to height"
+  (fact "crops the image to the given height"
+    ((crop-height-fn 200) test-image) => (dimensions-of [600 200]))
+
+  (fact "does not crop when the given height is bigger than images height"
+    ((crop-height-fn 12345) test-image) => (dimensions-of [600 314])))
