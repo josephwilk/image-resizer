e1ec11541df9b00b03c95661b3faab257cab86ff
Joscha <joscha@schnipseljagd.org>
2013-09-06 22:49:53 +0200

    add format as stream by mime-type

--- src/image_resizer/format.clj
+++ src/image_resizer/format.clj
@@ -12,7 +12,18 @@
     (ImageIO/write buffered-file (fs/extension file-with-path) resized-file)
     (.getAbsolutePath resized-file)))
 
-(defn as-stream [buffered-image mime-type]
+(defn as-stream
+  "image-format something like jpg, png.."
+  [buffered-image image-format]
   (let [baos (ByteArrayOutputStream.)]
-    (ImageIO/write buffered-image mime-type baos)
+    (ImageIO/write buffered-image image-format baos)
     (ByteArrayInputStream. (.toByteArray baos))))
+
+(defn as-stream-by-mime-type
+  "mime-type something like image/jpeg, image/png.."
+  [buffered-image mime-type]
+  (let [baos (ByteArrayOutputStream.)]
+    (let [writer (.next (ImageIO/getImageWritersByMIMEType mime-type))]
+        (.setOutput writer (ImageIO/createImageOutputStream baos))
+        (.write writer buffered-image)
+        (ByteArrayInputStream. (.toByteArray baos)))))
