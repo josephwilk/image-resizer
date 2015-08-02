c356511281b9d56c09cc75e882ee5a4dfeed8295
Guillermo Winkler <guillermo.winkler@gmail.com>
2015-08-02 11:54:23 -0300

    type hint image to be read as the read interface not a particular instance of it

--- src/image_resizer/util.clj
+++ src/image_resizer/util.clj
@@ -1,13 +1,13 @@
 (ns image-resizer.util
 (:import
-   [java.io File]
+   [java.io File Serializable]
    [javax.imageio ImageIO]
    [java.awt.image BufferedImage]))
 
 (defn buffered-image [image]
   (if (instance? BufferedImage image)
     image
-    (ImageIO/read ^java.io.ByteArrayInputStream image)))
+    (ImageIO/read ^java.io.Serializable image)))
 
 (defn dimensions [image]
   [(.getWidth image) (.getHeight image)])
