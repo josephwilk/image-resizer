6edf53ccc7d83a9337fc88ce48eab96de235a20b
Joseph Wilk <joe@josephwilk.net>
2017-07-25 11:54:53 +0100

    license

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject image-resizer "0.1.10"
   :description "Resize/Crop/Pad images"
   :url "https://github.com/josephwilk/image-resizer"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.imgscalr/imgscalr-lib "4.2"]]
 
